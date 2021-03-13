9c1fb23098a950a17a449a79fc246c0f7a99fb0d
Sara <Sara@192.168.56.1>
2021-03-13 13:49:37 +0100

    Inversions Implementation

--- src/main/java/algstudent/s32/Inversions.java
+++ src/main/java/algstudent/s32/Inversions.java
@@ -3,14 +3,57 @@ package algstudent.s32;
 import java.util.List;
 
 public class Inversions {
-//must have a O(nlogn)
+//must have a O(nlogn)-> for that the divide and conquer scheme must be a=b^k, using divide and conquer by division.
+	List<Integer> ranking;
+	
 	public Inversions(List<Integer> ranking) {
-		// TODO Auto-generated constructor stub
+		this.ranking=ranking;
 	}
 
 	public String start() {
-		// TODO Auto-generated method stub
-		return null;
+		long inversions=0;
+		inversions=countInversions(0,ranking.size()-1,this.ranking);
+		
+		return String.valueOf(inversions);
+	}
+
+	private long countInversions(int l, int r, List<Integer> ranking) {
+		long inversions=0;
+		if(r>l) {
+			int middle=(l+r)/2; 
+			countInversions(l,middle,ranking);
+			countInversions(middle+1, r, ranking);//a=2. k=1
+			inversions+=combineAll(l, middle, middle+1,r, ranking);
+		}
+		return inversions;
+	}
+
+	private long combineAll(int x1, int x2, int y1, int y2, List<Integer> ranking) {
+		long counter=0;
+		int sizeX=x2-x1+1;
+		int sizeY=y2-y1+1;
+		int x[]= new int[sizeX];
+		int y[]= new int[sizeY];
+		for (int i = 0; i < sizeX; i++) {
+			x[i]=ranking.get(x1+i);
+		}
+		for (int j = 0; j < sizeY; j++) {
+			x[j]=ranking.get(y1+j);
+		}
+		for (int i = 0; i <ranking.size(); i++) {
+			if(x[i]<y[i]) {
+				ranking.add(i,x[i]);
+				
+				//if the left number is smaller , theres no inversion.
+			}else if(y[i]<x[i]) {
+				ranking.add(i,y[i]);
+				counter++;//if a number at the right is smaller than a number at the left-> inversion
+			}
+		
+		}
+		
+		return counter;
+		
 	}
 
 }
