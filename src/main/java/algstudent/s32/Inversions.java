d2b485ed72e334f0b95e71cef931b0520ceb751d
Sara <Sara@192.168.56.1>
2021-03-14 13:52:30 +0100

    working on inversions

--- src/main/java/algstudent/s32/Inversions.java
+++ src/main/java/algstudent/s32/Inversions.java
@@ -11,26 +11,25 @@ public class Inversions {
 	}
 
 	public String start() {
-		long inversions = 0;
-		inversions = countInversions(0, ranking.size() - 1, this.ranking,0);
 
-		return String.valueOf(inversions);
+		return String.valueOf(countInversions(0, ranking.size() - 1, this.ranking, 0));
 	}
 
-	private long countInversions(int l, int r, List<Integer> ranking, int inversions ) {
-		
+	private long countInversions(int l, int r, List<Integer> ranking, int inversions) {
+
 		if (r > l) {
 			int middle = (l + r) / 2;
-			countInversions(l, middle, ranking,inversions);
-			countInversions(middle + 1, r, ranking,inversions);// a=2. k=1
+			inversions +=countInversions(l, middle, ranking, inversions);
+			inversions +=countInversions(middle + 1, r, ranking, inversions);// a=2. k=1
 			inversions += combineAll(l, middle, middle + 1, r, ranking);
+			
 		}
 		return inversions;
 	}
 
 	private long combineAll(int x1, int x2, int y1, int y2, List<Integer> ranking) {
 		// counter of inversions
-		long counter = 0;
+		long inversions = 0;
 		// computing the size of the auxiliar arrays,x and y .
 		int sizeX = x2 - x1 + 1;
 		int sizeY = y2 - y1 + 1;
@@ -47,31 +46,36 @@ public class Inversions {
 		}
 		int xi = 0;
 		int yi = 0;
+		boolean allEvaluated=false;
+		while (!allEvaluated) {
 
-		while (y[yi] == Integer.MAX_VALUE && x[xi] == Integer.MAX_VALUE) { // while there are values left to explore
-			if (x[xi] < y[yi]) {// if the value of xi is smaller than yi
+			// while there are values left to explore
+			if (x[xi] > y[yi]) {// if the value of xi is greater than yi, INVERSION
+				inversions++;
 				// since there's no need to sort, only to count the inversions, the only thing
 				// that happens
 				// here is that we increment the index or we set the array as visited.
-				if (xi == sizeX - 1) {
-					x[xi] = Integer.MAX_VALUE;// when there are no values of x left.
+				if (xi == sizeX - 1 || yi==sizeY-1) {
+					allEvaluated=true;
 				} else {
 
 					xi++;// if values of x are left
-				}
-			} else if (y[yi] < x[xi]) {// if the value of yi is smaller than xi-> disordered.
-				counter++;// if a value of y is smaller than the value of x , an inversion is produced
-				if (yi == sizeY - 1) {
-					y[yi] = Integer.MAX_VALUE;// when there are no values of y left.
-				} else {
-
-					yi++;// if values of y are left
+					yi++;
 				}
 			}
+//			} else if (y[yi] < x[xi]) {// if the value of yi is smaller than xi-> disordered.
+//				inversions++;// if a value of y is smaller than the value of x , an inversion is produced
+//				if (yi == sizeY - 1) {
+//					y[yi] = Integer.MAX_VALUE;// when there are no values of y left.
+//				} else {
+//
+//					yi++;// if values of y are left
+//				}
+//			}
 
 		}
-
-		return counter;
+		
+		return inversions;
 
 	}
 
