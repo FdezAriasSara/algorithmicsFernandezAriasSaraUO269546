431065888a2416a6724894445dbe5cff8891f877
Sara <Sara@192.168.56.1>
2021-03-16 00:49:24 +0100

    More work on inversions

--- src/main/java/algstudent/s32/Inversions.java
+++ src/main/java/algstudent/s32/Inversions.java
@@ -1,97 +1,82 @@
 package algstudent.s32;
 
-
 import java.util.List;
 
 public class Inversions {
 //must have a O(nlogn)-> for that the divide and conquer scheme must be a=b^k, using divide and conquer by division.
 	int[] ranking;
-	long inversions=0;
+	
 
 	public Inversions(List<Integer> ranking) {
-		this.ranking=new int[ranking.size()];
+		this.ranking = new int[ranking.size()];
 		for (int i = 0; i < ranking.size(); i++) {
-			this.ranking[i]=ranking.get(i);
+			this.ranking[i] = ranking.get(i);
 		}
 	}
 
 	public String start() {
-		
-		return String.valueOf(countInversions(0, this.ranking.length - 1, this.ranking));
+
+		return String.valueOf(sort(0, this.ranking.length - 1, this.ranking));
 	}
 
-	private long countInversions(int l, int r,  int[] ranking) {
-
+	private long sort(int l, int r, int[] ranking) {
+		long inversions = 0;
 		if (r > l) {
 			int middle = (l + r) / 2;
-			countInversions(l, middle, ranking);
-			countInversions(middle + 1, r, ranking);// a=2. k=1 (since combine all has a complexity of O(n)) and b=2 
-			combineAll(l, middle, middle + 1, r);
-			
-		}
-		return this.inversions;
-	}
+			//two recursive calls=> a=2 . Each time the vector is divided in half its size ,
+			//then b=2. The rest of the calls provide a complexity of O(n) and so k=1.
+			//therefore O(n^k*logn)=(nlogn)
 
-	private void combineAll(int x1, int x2, int y1, int y2) {
-		
-		
+			inversions += sort(middle + 1, r, ranking);
+			inversions += sort(l, middle, ranking);
+			inversions += inversions(l, middle, middle + 1, r, ranking);
+		}
+
+		return inversions;
+	}
+/**
+ * Method that compares the two halves of the array passed as parameter and counts the number of inversions found when comparing them.
+ * @return the number of inversions counted
+ */
+	private long inversions(int left, int middle, int mid1, int right, int[] ranking) {
+
+		long inversions = 0;
 		// computing the size of the auxiliar arrays,x and y .
-		int sizeX = x2 - x1 + 1;
-		int sizeY = y2 - y1 + 1;
+		int sizeX = middle - left + 1;
+		int sizeY = right - mid1 + 1;
 		// auxiliar array initialization
 		int x[] = new int[sizeX];
 		int y[] = new int[sizeY];
-		//auxiliar sorted array
-		
-		int sorted[] = new int[sizeX+sizeY];
-   
+
 		// filling the auxiliar arrays with the elements to be evaluated.
 		for (int i = 0; i < sizeX; i++) {
-			x[i] = this.ranking[x1 + i];
+			x[i] = ranking[left + i];
 		}
 		for (int j = 0; j < sizeY; j++) {
-			y[j] =this.ranking[y1 + j];
+			y[j] = ranking[mid1 + j];
 		}
-		//for the while loop
+
+		// for the while loop
 		int xi = 0;
-		int yi = 0;	
-		int si=0;//sorted array.
-		
-		while (xi<sizeX && yi<sizeY) { // while there are values left to explore
-			if(x[xi]<=y[yi]) {				
-				sorted[si]= x[xi];				
-				xi++;//we take the next value of array x, since the current one was already stored in sorted.
-				si++;
-				//we don't increment the index for array x since the element has not been stored yet.
-			}else if(x[xi]>=y[yi]) {
-				this.inversions+=1;
-				sorted[si]= y[yi];
-					yi++;//we take the next value of array y, since the current one was already stored in sorted.
-					si++;
-				}
-			//adding the left elements once all the smaller ones have been added.
-			else if(yi<sizeY) {
-				sorted[si]=yi;
-				yi++;
-				si++;
-				
-			}
-			else {
-				this.inversions+=1;
-				sorted[si]=xi;
+		int yi = 0;
+
+		while ((xi < sizeX) && (yi < sizeY)) { // while there are values left to explore
+			if (x[xi] <= y[yi]) {
+				// we take the next value of array x, since the current one was already stored
+				// in sorted.
 				xi++;
-				si++;
+				// we don't increment the index for array y since the element has not been
+				// stored yet.
+			} else {
+				inversions += (middle - xi);
+				yi++;// we take the next value of array y, since the current one was already stored
+						// in sorted.
+
 			}
-			}
-			int ri=0;
-			si=0;
-			while (si<sorted.length) {
-				this.ranking[ri]=sorted[si];
-				si++;
-				ri++;
-			}
-			
-	
+
+		}
+
+		return inversions;
 	}
 
 }
--- src/main/java/algstudent/s32/InversionsTimes.java
+++ src/main/java/algstudent/s32/InversionsTimes.java
@@ -14,10 +14,10 @@ public class InversionsTimes {
 	public static void main(String args[]) { 
 		int numberFiles = 7;
 		
-		//for (int i = 0; i <= numberFiles; i++) {
+		for (int i = 1; i <= numberFiles; i++) {
 			//Check if the path is correct for you
-			//String fileName = Paths.get("").toAbsolutePath().toString() + "/src/main/java/algstudent/s32/ranking" + i + ".txt";
-		String fileName = Paths.get("").toAbsolutePath().toString() + "/src/main/java/algstudent/s32/ranking" + 8 + ".txt";
+		//	String fileName = Paths.get("").toAbsolutePath().toString() + "/src/main/java/algstudent/s32/ranking" + i + ".txt";
+		String fileName = Paths.get("").toAbsolutePath().toString() + "/src/main/java/algstudent/s32/ranking" + i+ ".txt";
 		System.out.println("FILE: " + fileName);
 			
 			ranking = readRankingFromFile(fileName);
@@ -25,18 +25,21 @@ public class InversionsTimes {
 	        long  t1 = System.currentTimeMillis();              
 	        System.out.println("Number of inversions = " + inv1.start());                 				
 	        long  t2 = System.currentTimeMillis();              
-	        //if(i>1) System.out.println("The time for the algorithm O(n logn) is: " + (t2-t1) + " milliseconds");
-	        System.out.println("The time for the algorithm O(n logn) is: " + (t2-t1) + " milliseconds");
+	        if(i>1) System.out.println("The time for the algorithm O(n logn) is: " + (t2-t1) + " milliseconds");
+	       // System.out.println("The time for the algorithm O(n logn) is: " + (t2-t1) + " milliseconds");
 	
 	        ranking = readRankingFromFile(fileName);
+	       // System.out.println(ranking);
 	        InversionsQuadratic inv2 = new InversionsQuadratic(ranking);	
 	        t1 = System.currentTimeMillis();              
 	        System.out.println("Number of inversions = " + inv2.start());                 				
 	        t2 = System.currentTimeMillis();              
-	       // if(i>1)  System.out.println("The time for the algorithm O(n^2) is: " + (t2-t1) + " milliseconds");
-	        System.out.println("The time for the algorithm O(n^2) is: " + (t2-t1) + " milliseconds");
+	        if(i>1)  System.out.println("Thes time for the algorithm O(n^2) is: " + (t2-t1) + " milliseconds");
+	        //System.out.println("The time for the algorithm O(n^2) is: " + (t2-t1) + " milliseconds");
 	        System.out.println("\n****************************\n");
-		//}	
+		//
+	        
+		}	
 
 	}
 	
