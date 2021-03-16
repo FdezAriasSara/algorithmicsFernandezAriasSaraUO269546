18977d5b30561e1508ba15072ae810247e419fb5
Sara <Sara@192.168.56.1>
2021-03-16 22:26:07 +0100

    Realized I was not sorting the vector so it wouldnt work

--- src/main/java/algstudent/s32/Inversions.java
+++ src/main/java/algstudent/s32/Inversions.java
@@ -16,10 +16,10 @@ public class Inversions {
 
 	public String start() {
 
-		return String.valueOf(sort(0, this.ranking.length - 1, this.ranking));
+		return String.valueOf(sort(0, this.ranking.length - 1));
 	}
 
-	private long sort(int l, int r, int[] ranking) {
+	private long sort(int l, int r) {
 		long inversions = 0;
 		if (r > l) {
 			int middle = (l + r) / 2;
@@ -27,9 +27,9 @@ public class Inversions {
 			//then b=2. The rest of the calls provide a complexity of O(n) and so k=1.
 			//therefore O(n^k*logn)=(nlogn)
 
-			inversions += sort(middle + 1, r, ranking);
-			inversions += sort(l, middle, ranking);
-			inversions += inversions(l, middle, middle + 1, r, ranking);
+			inversions += sort(middle + 1, r);
+			inversions += sort(l, middle);
+			inversions += inversions(l, middle, middle + 1, r);
 		}
 
 		return inversions;
@@ -38,45 +38,63 @@ public class Inversions {
  * Method that compares the two halves of the array passed as parameter and counts the number of inversions found when comparing them.
  * @return the number of inversions counted
  */
-	private long inversions(int left, int middle, int mid1, int right, int[] ranking) {
+	private long inversions(int left, int middle, int mid1, int right) {
 
 		long inversions = 0;
 		// computing the size of the auxiliar arrays,x and y .
-		int sizeX = middle - left + 1;
-		int sizeY = right - mid1 + 1;
+		int sizeLeft = middle - left + 1;
+		int sizeRight = right - mid1 + 1;
 		// auxiliar array initialization
-		int x[] = new int[sizeX];
-		int y[] = new int[sizeY];
+		int lArray[] = new int[sizeLeft];
+		int rArray[] = new int[sizeRight];
 
 		// filling the auxiliar arrays with the elements to be evaluated.
-		for (int i = 0; i < sizeX; i++) {
-			x[i] = ranking[left + i];
+		for (int i = 0; i < sizeLeft; i++) {
+			lArray[i] = ranking[left + i];
 		}
-		for (int j = 0; j < sizeY; j++) {
-			y[j] = ranking[mid1 + j];
+		for (int j = 0; j < sizeRight; j++) {
+			rArray[j] = ranking[mid1 + j];
 		}
 
 		// for the while loop
-		int xi = 0;
-		int yi = 0;
-
-		while ((xi < sizeX) && (yi < sizeY)) { // while there are values left to explore
-			if (x[xi] <= y[yi]) {
+		int leftIndex = 0;
+		int rightIndex = 0;
+		int rankingIndex=left;
+		while ((leftIndex < lArray.length) && (rightIndex <rArray.length)) { // while there are values left to explore
+			if (lArray[leftIndex] <= rArray[rightIndex]) {
+				ranking[rankingIndex]=lArray[leftIndex];
 				// we take the next value of array x, since the current one was already stored
 				// in sorted.
-				xi++;
+				rankingIndex++;
+			    leftIndex++;
 				// we don't increment the index for array y since the element has not been
 				// stored yet.
 			} else {
-				inversions =inversions+(mid1-xi);
-				yi++;// we take the next value of array y, since the current one was already stored
+				ranking[rankingIndex]=rArray[rightIndex];
+				inversions+=mid1-(left+ leftIndex);//The elements from mid1 to the elment where the inversion was detected will produce an inversion too
+				rankingIndex++;
+				rightIndex++;// we take the next value of array y, since the current one was already stored
 						// in sorted.
 
 			}
 
 		}
+		//inserting into the ranking the values that are left both from x and y.
+		while (leftIndex < lArray.length) {
+			
+			ranking[rankingIndex] = lArray[leftIndex];
+			rankingIndex++;
+			leftIndex++;
+		}
+	    while (rightIndex < rArray.length) {
+	    	ranking[rankingIndex] = rArray[rightIndex];
+	    	rankingIndex++;
+			rightIndex++;
+	    }
+	    	
 
 		return inversions;
 	}
-
+	
+	
 }
