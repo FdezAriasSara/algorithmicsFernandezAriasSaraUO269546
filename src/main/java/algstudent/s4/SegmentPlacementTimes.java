fbd9fb5abe4eb122d8ce3d36fb9af440f3c5d441
SARA FERNANDEZ ARIAS <UO269546@uniovi.es>
2021-03-23 15:33:58 +0100

    Time measuring

--- src/main/java/algstudent/s4/SegmentPlacementTimes.java
+++ src/main/java/algstudent/s4/SegmentPlacementTimes.java
@@ -1,5 +1,71 @@
 package algstudent.s4;
 
-public class SegmentPlacementTimes {
 
+
+public class SegmentPlacementTimes {
+	public static final int GREEDY_1 = 1;
+	public static final int GREEDY_2 = 2;
+	public static final int GREEDY_3 = 3;
+	static int nTimes=40000;
+	private static SegmentsPlacement segPlacement;
+	
+	public static void main(String[] args) {
+		long t;
+		for (int n = 100; n <Integer.MAX_VALUE; n *= 2) {
+			System.out.println(String.format("For a size of n=%d",n));
+				t=measureTimes(nTimes,GREEDY_1, n);
+				System.out.println(String.format("-Time taken for greedy1 :%d ms", t));
+				t=measureTimes(100, GREEDY_2, n);
+				System.out.println(String.format("-Time taken for greedy2 :%d ms", t));
+				t=measureTimes(100, GREEDY_3, n);
+				System.out.println(String.format("-Time taken for greedy3 :%d ms", t));
+				System.out.println(
+						"##################################################################################################");
+
+		}
+
+	}
+
+	public static long measureTimes(int iterations,int greedy ,int n) {
+		segPlacement=new SegmentsPlacement(n);
+		long t=0,t1,t2;
+		switch (greedy) {
+		case GREEDY_1:
+			
+			for (int i = 1; i <= iterations ; i++) {
+				t1 = System.currentTimeMillis();
+				segPlacement.greedy1();
+				t2 = System.currentTimeMillis();
+				t += t2-t1;
+				
+			}
+			return t;
+		case GREEDY_2:		
+			for (int i = 1; i <= iterations ; i++) {
+				t1 = System.currentTimeMillis();
+				segPlacement.greedy2();
+				t2 = System.currentTimeMillis();
+				t += t2-t1;
+				
+			}
+			return t;
+		case GREEDY_3:
+			
+			for (int i = 1; i <= iterations ; i++) {
+				t1 = System.currentTimeMillis();
+				segPlacement.greedy3();
+				t2 = System.currentTimeMillis();
+				t += t2-t1;
+				
+			}
+			return t;
+
+		default:
+			System.out.println("An error happened.");
+			return -1;
+		}
+		
+	
+		
+	}
 }
--- src/main/java/algstudent/s4/SegmentsPlacement.java
+++ src/main/java/algstudent/s4/SegmentsPlacement.java
@@ -8,54 +8,74 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class SegmentsPlacement {
 	List<Integer> segments;
 	int numberOfSegments;
-	
+	Random rn;
+
 	public SegmentsPlacement(String filename) {
-		
-		this.segments=readGameFromFile(filename);
+
+		this.segments = readGameFromFile(filename);
 	}
+
+	public SegmentsPlacement(int n) {
+		this.segments=new ArrayList<Integer>();
+		for (int i = 0; i < n; i++) {
+			rn=new Random();
+			this.segments.add(i,rn.nextInt());
+		}
+	}
+
 	public static void main(String args[]) {
 		long total;
-		//###############################################GAME 1#################################################
+		// ###############################################GAME
+		// 1#################################################
 		String fileName = Paths.get("").toAbsolutePath().toString() + "/src/main/java/algstudent/s4/game1.txt";
-		SegmentsPlacement segPlace= new SegmentsPlacement(fileName);
-		System.out.println("###############################################GAME 1#################################################");
-		System.out.println("The solution using the ostrich algorithim  (greedy 1) is :");             
-        total=segPlace.greedy1();                				
-        System.out.println("Total cost of greedy1 :"+total);    
-        
-        System.out.println("--------------------------------------------------------------------------------------------------");
-        System.out.println("The solution using the greedy 2 is :");             
-        total=segPlace.greedy2();                				
-        System.out.println("Total cost of greedy2 :"+total);    
-        
-        System.out.println("--------------------------------------------------------------------------------------------------");
-        
-        System.out.println("The solution using the algorithim  greedy3 is :");             
-        total=segPlace.greedy3();                				
-        System.out.println("Total cost of greedy3 :"+total);    
-        System.out.println("###############################################GAME 2#################################################");
-		//###############################################GAME 2#################################################
-         fileName = Paths.get("").toAbsolutePath().toString() + "/src/main/java/algstudent/s4/game2.txt";
-		 segPlace= new SegmentsPlacement(fileName);
-		System.out.println("The solution using the algorithim  (greedy 1) is :");             
-        total=segPlace.greedy1();                				
-        System.out.println("Total cost of greedy1 :"+total);    
-        
-        System.out.println("--------------------------------------------------------------------------------------------------");
-        System.out.println("The solution using the greedy 2 is :");             
-        total=segPlace.greedy2();                				
-        System.out.println("Total cost of greedy2 :"+total);    
-        
-        System.out.println("--------------------------------------------------------------------------------------------------");
-        
-        System.out.println("The solution using the algorithim  greedy3 is :");             
-        total=segPlace.greedy3();                				
-        System.out.println("Total cost of greedy3 :"+total);    
+		SegmentsPlacement segPlace = new SegmentsPlacement(fileName);
+		System.out.println(
+				"###############################################GAME 1#################################################");
+		System.out.println("The solution using the ostrich algorithim  (greedy 1) is :");
+		total = segPlace.greedy1();
+		System.out.println("Total cost of greedy1 :" + total);
+
+		System.out.println(
+				"--------------------------------------------------------------------------------------------------");
+		System.out.println("The solution using the greedy 2 is :");
+		total = segPlace.greedy2();
+		System.out.println("Total cost of greedy2 :" + total);
+
+		System.out.println(
+				"--------------------------------------------------------------------------------------------------");
+
+		System.out.println("The solution using the algorithim  greedy3 is :");
+		total = segPlace.greedy3();
+		System.out.println("Total cost of greedy3 :" + total);
+		System.out.println(
+				"###############################################GAME 2#################################################");
+		// ###############################################GAME
+		// 2#################################################
+		fileName = Paths.get("").toAbsolutePath().toString() + "/src/main/java/algstudent/s4/game2.txt";
+		segPlace = new SegmentsPlacement(fileName);
+		System.out.println("The solution using the algorithim  (greedy 1) is :");
+		total = segPlace.greedy1();
+		System.out.println("Total cost of greedy1 :" + total);
+
+		System.out.println(
+				"--------------------------------------------------------------------------------------------------");
+		System.out.println("The solution using the greedy 2 is :");
+		total = segPlace.greedy2();
+		System.out.println("Total cost of greedy2 :" + total);
+
+		System.out.println(
+				"--------------------------------------------------------------------------------------------------");
+
+		System.out.println("The solution using the algorithim  greedy3 is :");
+		total = segPlace.greedy3();
+		System.out.println("Total cost of greedy3 :" + total);
 	}
+
 	/**
 	 * Applying the ostrich algorithm. The segments are placed in the same order in
 	 * which they appear in the file and in the end every solution gives the same
@@ -64,107 +84,114 @@ public class SegmentsPlacement {
 	 * @return
 	 */
 	public long greedy1() {
-		int[] nonSorted=copyToArray(segments);
-		printSolution(nonSorted);
+		int[] nonSorted = copyToArray(segments);
+		//printSolution(nonSorted);
 		return computeCost(nonSorted);
 	}
+
 	private void printSolution(int[] elements) {
-		
-			
-			
-		int start=0;
+
+		int start = 0;
 		int end;
 		long midpoint;
 		for (int i = 0; i < elements.length; i++) {
-			end=start+elements[i];
-			midpoint=(start+end)/2;
-			System.out.println(String.format("(%d,%d) midpoint:%d",start,end,midpoint));
-			start=end;
+			end = start + elements[i];
+			midpoint = (start + end) / 2;
+			System.out.println(String.format("(%d,%d) midpoint:%d", start, end, midpoint));
+			start = end;
 		}
-			
-		}
-	
+
+	}
+
 	/**
-	 *Placing them from longest to shortest length.
+	 * Placing them from longest to shortest length.
 	 * 
 	 * @return
 	 */
 	public long greedy2() {
-		
-		int[] sorted=sortDescendantOrder();
-		printSolution(sorted);
+
+		int[] sorted = sortDescendantOrder();
+	//	printSolution(sorted);
 		return computeCost(sorted);
 	}
+
 	/**
-	* From shortest to longest length.
+	 * From shortest to longest length.
+	 * 
 	 * @return
 	 */
 	public long greedy3() {
-		int[] sorted=sortAscendantOrder();
-		printSolution(sorted);
+		int[] sorted = sortAscendantOrder();
+	//	printSolution(sorted);
 		return computeCost(sorted);
 	}
-/**
- * Copies elements from an list of integers into an array in the same exact order they appear.
- * @param list
- * @return the elements in array.
- */
+
+	/**
+	 * Copies elements from an list of integers into an array in the same exact
+	 * order they appear.
+	 * 
+	 * @param list
+	 * @return the elements in array.
+	 */
 	private int[] copyToArray(List<Integer> list) {
-		int[] toReturn=new int[list.size()];
+		int[] toReturn = new int[list.size()];
 		for (int i = 0; i < toReturn.length; i++) {
-			toReturn[i]=list.get(i);
+			toReturn[i] = list.get(i);
 		}
 		return toReturn;
 	}
+
 	/**
 	 * Computes the cost in PUFOSOS of segments by the addition of their midpoints.
+	 * 
 	 * @param arrayOfSegments
 	 * @return the cost of a game.
 	 */
 	private long computeCost(int[] arrayOfSegments) {
-		int start=0;
-		long cost=0;
+		int start = 0;
+		long cost = 0;
 		int end;
-		for (int si = 0; si <arrayOfSegments.length-1; si++) {
-			end=start+arrayOfSegments[si ];
-			cost+=(start+end)/2;
-			start=end;
+		for (int si = 0; si < arrayOfSegments.length - 1; si++) {
+			end = start + arrayOfSegments[si];
+			cost += (start + end) / 2;
+			start = end;
 		}
 		return cost;
 	}
 
 	private int[] sortAscendantOrder() {
-		List<Integer> sorted=new ArrayList<Integer>(segments);//Copy constructor
+		List<Integer> sorted = new ArrayList<Integer>(segments);// Copy constructor
 
-		 for (int i= 0 ; i < sorted.size(); i++) { 
-	            for (int j = i + 1; j < sorted.size(); j++) { 
-	                if (sorted.get(i) > sorted.get(j)) 
-	                {
-	                	Collections.swap(sorted, i, j);
+		for (int i = 0; i < sorted.size(); i++) {
+			for (int j = i + 1; j < sorted.size(); j++) {
+				if (sorted.get(i) > sorted.get(j)) {
+					Collections.swap(sorted, i, j);
 
-	                }
-	            }
-	        
-		}System.out.println(sorted);
-		return copyToArray(sorted);
+				}
+			}
+
+		}
 		
-	}
-	private int[] sortDescendantOrder() {
-	List<Integer> sorted=new ArrayList<Integer>(segments);//Copy constructor
-	
-		 for (int i= 0 ; i < sorted.size(); i++) { 
-	            for (int j = i + 1; j < sorted.size(); j++) { 
-	                if (sorted.get(i) < sorted.get(j)) 
-	                {
-	                	Collections.swap(sorted, i, j);
+		return copyToArray(sorted);
 
-	                }
-	            }
-	        
-		}System.out.println(sorted);
+	}
+
+	private int[] sortDescendantOrder() {
+		List<Integer> sorted = new ArrayList<Integer>(segments);// Copy constructor
+		
+		for (int i = 0; i < sorted.size(); i++) {
+		
+			for (int j = i + 1; j < sorted.size(); j++) {
+				if (sorted.get(i) < sorted.get(j)) {
+					Collections.swap(sorted, i, j);
+
+				}
+			}
+
+		}
+		
 		return copyToArray(sorted);
 	}
-	
 
 	// inspired in readRankingFromFile of session3.
 	public List<Integer> readGameFromFile(String file) {
@@ -174,9 +201,9 @@ public class SegmentsPlacement {
 
 		try {
 			fich = new BufferedReader(new FileReader(file));
-			line=fich.readLine();//first line tells the number of segments
-			this.numberOfSegments = Integer.parseInt(line); 
-			while (line != null && numberOfSegments!=0) {
+			line = fich.readLine();// first line tells the number of segments
+			this.numberOfSegments = Integer.parseInt(line);
+			while (line != null && numberOfSegments != 0) {
 				segments.add(Integer.parseInt(line));
 				line = fich.readLine();
 			}
