f294c200a5e070986bc7f1d3a8cd98629c9dfc8c
SARA FERNANDEZ ARIAS <UO269546@uniovi.es>
2021-04-07 15:20:50 +0200

    Session 6: Backtracking

---
+++ src/main/java/examples/PasswordGeneratorTest.java
@@ -0,0 +1,30 @@
+package examples;
+
+import java.util.List;
+import org.junit.Test;
+
+public class PasswordGeneratorTest {
+	@Test
+	public void test() {
+		int numberOfTotalCharacters = 100;
+		int numberOfNonLettersEnds = 2;
+		int numberOfPasswords = 1000;
+		String consonantPairsPath = "src/main/java/labs/examples/backtracking/passwords/consonant_pairs.txt"; 
+		
+		PasswordGenerator generator = new PasswordGenerator(
+				numberOfTotalCharacters,
+				numberOfNonLettersEnds,
+				numberOfPasswords,
+				consonantPairsPath);
+	
+		generator.generate();
+		
+		List<String> passwords = generator.getPasswords();	
+		
+		for (int i = 0; i < numberOfPasswords; i++) {			
+			String password = passwords.get(i);
+			System.out.println((i+1) + ": " + password);
+		}
+	}
+	
+}
